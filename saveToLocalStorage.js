cf18d2796b826e2212713b82ea04d91483311428
Kehinde Adedokun <75108469+hi-heavens@users.noreply.github.com>
2023-04-09 14:35:36 +0100

    save and retrieve from the local storage

---
+++ saveToLocalStorage.js
@@ -0,0 +1,13 @@
+// Save a value to localStorage
+// Delete your code and refresh the page
+// Fetch your value from localStorage and log it out
+
+// save a value to localStorage
+/* 
+const myName = "Alvicci";
+localStorage.setItem("myName", myName);
+console.log(localStorage.getItem("myName"));
+*/
+
+// retrieve a value from localStorage
+console.log(localStorage.getItem("myName"));
